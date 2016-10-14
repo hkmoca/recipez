239cc8d863a6f465e6e385a0d50c8b66743bd7d7
Héctor Moreno <hmoreno@nearsoft.com>
2016-10-14 15:11:50 -0700

    Configurate tableView

--- recipez/ViewController.swift
+++ recipez/ViewController.swift
@@ -12,9 +12,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
+    var recipes = [Recipe]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -22,10 +26,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return recipes.count
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        if let cell = tableView.dequeueReusableCellWithIdentifier("RecipeCell") as? RecipeCell {
+        
+            let recipe = recipes[indexPath.row]
+            cell.configureCell(recipe)
+            return cell
+        } else {
+            return RecipeCell()
+        }
+        
+        
     }
     
     
